10b235a32416a42fe36473345fed123fc69e5a39
yunfeiWu97 <wuyunfei97@gmail.com>
2026-01-23 15:48:10 -0600

    lift employees into state and add employees via form.

--- fs_lab-1.2/src/App.tsx
+++ fs_lab-1.2/src/App.tsx
@@ -1,12 +1,30 @@
 import "./App.css";
-import { employees } from "./data/employees";
+import { useState } from "react";
+import { employees as employeeData } from "./data/employees";
 import { groupByDepartment } from "./utils/groupByDepartment";
 import { DepartmentSection } from "./components/DepartmentSection";
 import { SiteFooter } from "./components/SiteFooter";
 import { SiteHeader } from "./components/SiteHeader";
+import { AddEmployeeForm } from "./components/AddEmployeeForm";
+import type { Employee } from "./types/employee";
 
 export default function App() {
+  const [employees, setEmployees] = useState<Employee[]>(employeeData);
+
   const departments = groupByDepartment(employees);
+
+  const departmentNames = departments.map((d) => d.name);
+
+  const handleAddEmployee = (firstName: string, department: string) => {
+    setEmployees((oldEmployees) => {
+      const newEmployee: Employee = {
+        firstName: firstName,
+        department: department,
+      };
+
+      return [...oldEmployees, newEmployee];
+    });
+  };
 
   return (
     <div className="page">
@@ -16,6 +34,11 @@ export default function App() {
         {departments.map((department) => (
           <DepartmentSection key={department.name} department={department} />
         ))}
+
+        <AddEmployeeForm
+          departments={departmentNames}
+          onAdd={handleAddEmployee}
+        />
       </main>
 
       <SiteFooter />
